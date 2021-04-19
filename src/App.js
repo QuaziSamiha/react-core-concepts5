79b45d07e3e2e720861b794da78474de44f6c26c
QuaziSamiha <1703155@student.ruet.ac.bd>
2021-04-19 23:50:38 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'; 
 
 function App() {
   return (
     <div className="App">
+      
+      <MovieCounter></MovieCounter>
+      <MovieCounter></MovieCounter>
+      <MovieCounter></MovieCounter>
+      <MovieCounter></MovieCounter>
+      
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
+
+function MovieCounter() {
+
+  // const total = useState(0);
+  // console.log(total);
+
+  let [count, setCount] = useState(0);
+
+  const handleClick = () => setCount(count + 1);
+
+  const divStyle = {
+    border: '3px solid gray',
+    borderRadius: '10px',
+    margin: '10px',
+    width: '200px'
+  }
+  const btnStyle = {
+    padding: '5px',
+    color: 'lightgray',
+    backgroundColor: 'salmon',
+    marginBottom: '8px'
+  }
+  return (
+    <div style={divStyle}>
+      <h5>Number of movies: {count}</h5>
+      <button style={btnStyle} onClick={handleClick}>Add Movie</button>
+      <MovieDisplay movies={count + 3}></MovieDisplay>
+      <MovieDisplay movies={count + 10}></MovieDisplay>
+    </div>
+  )
+}
+
+function MovieDisplay(props){
+  return (
+    <div>
+      <h4>Movies I have acted: {props.movies}</h4>
+    </div>
+  )
+}
+
 
 export default App;
